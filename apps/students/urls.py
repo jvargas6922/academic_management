57ea15b8a50c4b53effca00b74ee6cf18dedd730
adrian6922 <vargaszambranoj@gmaail.com>
2026-02-18 20:39:48 -0300

    crud terminado de estudiantes

--- apps/students/urls.py
+++ apps/students/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='list_students'),
     path('create/', views.create, name='create_student'),
+    path('edit/<int:student_id>/', views.edit, name='edit_student'),
+    path('update/<int:student_id>/', views.update, name='update_student'),
+    path('delete/<int:student_id>/', views.delete, name='delete_student'),
 ]
--- apps/students/views.py
+++ apps/students/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
 from .models import Student
+
 
 # Create your views here.
 def index(requests):
@@ -34,5 +35,40 @@ def create(requests):
             messages.error(requests, f"Error al crear el estudiante: {str(e)}")
         
     return render(requests, 'student/create.html')
+
+def edit(requests, student_id):
+    # este metodo me retorna el objeto o un error 404 si no lo encuentra
+    student = get_object_or_404(Student, id=student_id)
+    return render(requests, 'student/edit.html', {'student': student})
+
+def update(requests, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    if requests.method == "POST":
+        try:
+            # captura de los datos del formulario
+            first_name = requests.POST["first_name"] # otra forma de escribirlo
+            last_name = requests.POST["last_name"]
+            email = requests.POST["email"]
+            birth_date = requests.POST["birth_date"]
+            # actualización de los campos del estudiante
+            student.first_name = first_name
+            student.last_name = last_name
+            student.email = email
+            student.birth_date = birth_date
+            student.save()
+            messages.success(requests, "Estudiante actualizado exitosamente")
+            return redirect('list_students')
+        except Exception as e:
+            messages.error(requests, f"Error al actualizar el estudiante: {str(e)}")
+    return render(requests, 'student/edit.html', {'student': student})
+
+def delete(requests, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    try:
+        student.delete()
+        messages.success(requests, "Estudiante eliminado exitosamente")
+        return redirect('list_students')
+    except Exception as e:
+        messages.error(requests, f"Error al eliminar el estudiante: {str(e)}")
 
         
